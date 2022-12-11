5b423d267f0f56d58b71eacdda28eaf34fba36ff
stellakr <112539195+stellakr@users.noreply.github.com>
2022-12-11 14:36:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+(function(){
+
+    const canvas = document.querySelector('canvas'),
+    ctx = canvas.getContext('2d');
+
+    let cardImage = new Image();
+    cardImage.src = 'https://www.calculatorsoup.com/images/deck_new/poker_deck/AH.svg'
+    console.log(cardImae);
+
+    let AceHearts = {
+        img: cardImage,
+        x: 0,
+        y: 0
+    }
+}
